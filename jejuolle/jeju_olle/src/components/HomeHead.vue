adaef90d55f0c117170f4c7378cb0e04e386775f
김젼이 <wlgus0321@gmail.com>
2022-08-18 10:09:45 +0900

    jeju olle fe
    home title, home body 작성중

---
+++ jejuolle/jeju_olle/src/components/HomeHead.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="background">
+        <p class="title"> JEJU Olle? </p>
+    </div>
+
+    
+</template>
+
+<script>
+    export default{
+    }
+</script>
+
+<style scoped>
+    .background {
+    height: 250px;
+    overflow: hidden;
+    margin:0;
+    background-image: url("https://images.unsplash.com/photo-1596941248238-0d49dcaa4263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    }
+    .title {
+        
+        position: absolute;
+        width: 329px;
+        height: 94px;
+        left: 60px;
+        top: 122px;
+
+        font-family: 'Rubik', sans-serif;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 50px;
+        line-height: 62px;
+
+        color: #FFFFFF;
+
+    }
+</style>
